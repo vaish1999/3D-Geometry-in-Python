58c3d050ccfc90e34b8e7fc1f873f02f991012b7
Vaishakh <vaishakh.nargund1999@gmail.com>
2020-06-21 02:54:27 +0530

    removed

--- 3dg/__init__.py
+++
@@ -1,4 +0,0 @@
-from math import acos,pi,asin
-from 3dg.distance import lineToLine,pointToLine,pointToPlane,p2p
-from 3dg.vector import magnitude,dotProduct,crossProduct
-from 3dg.angle import lines,planes,lineAndPlane
