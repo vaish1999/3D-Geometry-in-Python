c5e2980edc1bfaf52742fc4d6eb7fd965c7de7c8
Vaishakh <vaishakh.nargund1999@gmail.com>
2020-06-21 02:28:28 +0530

    updated

--- 3dg/__init__.py
+++ 3dg/__init__.py
@@ -1,5 +1,4 @@
 from math import acos,pi,asin
-dg = __import__('3dg')
-from dg.distance import lineToLine,pointToLine,pointToPlane,p2p
-from dg.vector import magnitude,dotProduct,crossProduct
-from dg.angle import lines,planes,lineAndPlane
+from 3dg.distance import lineToLine,pointToLine,pointToPlane,p2p
+from 3dg.vector import magnitude,dotProduct,crossProduct
+from 3dg.angle import lines,planes,lineAndPlane
