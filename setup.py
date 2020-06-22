10cf715a01cd6ef260ecff874737bf2793cffb56
Vaishakh <vaishakh.nargund1999@gmail.com>
2020-06-22 12:53:21 +0530

    updated

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 setup(
   name = 'd3g',         
   packages = ['d3g'],   
-  version = '0.3',      
+  version = '0.4',      
   license='GPL 2.0',        
   description = 'Explore 3D geometry in python over here!',   
   author = 'Vaishakh Nargund',                  
@@ -15,7 +15,7 @@ setup(
   long_description=long_description,
   long_description_content_type="text/markdown",     
   url = 'https://github.com/vaish1999/3D-Geometry-in-Python',  
-  download_url = 'https://github.com/vaish1999/3D-Geometry-in-Python/archive/v2.2.tar.gz',   
+  download_url = 'https://github.com/vaish1999/3D-Geometry-in-Python/archive/v2.3.tar.gz',   
   keywords = ['3d', 'geometry', 'line','plane','distance','python','vision'],   
   install_requires=[],
   classifiers=[
